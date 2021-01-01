2b431c5133277ec0165377d9fa0021c72863270a
bwltoh <76431127+bwltoh@users.noreply.github.com>
2021-01-01 13:06:47 -0800

    Update Constants.java

--- app/src/main/java/com/example/e_commerce_store/utils/Constants.java
+++ app/src/main/java/com/example/e_commerce_store/utils/Constants.java
@@ -2,7 +2,7 @@ package com.example.e_commerce_store.utils;
 
 public class Constants {
 
-    public static String LOCALHOST = "http://192.168.43.3:8000/api/";//http://localhost:8000/api/
+    public static String LOCALHOST = "http://localhost:8000/api/";
     public static String API_KEY="hSHUOK89uTTNeAPzDWJv1Edf9";
     public static String USER_NAME="user_name";
     public static String USER_EMAIL="user_email";
